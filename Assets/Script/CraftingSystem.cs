2d66d60acb22903c507b3ed8f390521b52bc0f7c
wonhwo <dlgy9714@gmail.com>
2023-06-13 13:55:01 +0900

    첫 커밋

---
+++ Assets/Script/CraftingSystem.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingSystem : MonoBehaviour
+{
+    public GameObject craftingScreenUI;
+    public GameObject toolsScreenUI;
+
+    public List<string> inventoryItemList = new List<string>();
+
+    Button toolsBTN;
+
+    Button craftAxeBTN;
+
+    Text AxeReq1, AxeReq2;
+    public bool isOpen;
+    public static CraftingSystem Instance { get; set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        isOpen = false;
+
+        toolsBTN = craftingScreenUI.transform.Find("ToolsButton").GetComponent<Button>();
+        toolsBTN.onClick.AddListener(delegate { OpenToolsCategory(); });
+
+        AxeReq1 = toolsScreenUI.transform.Find("Axe").transform.Find("req1").GetComponent<Text>();
+        AxeReq2 = toolsScreenUI.transform.Find("Axe").transform.Find("req2").GetComponent<Text>();
+
+        craftAxeBTN = toolsScreenUI.transform.Find("Axe").transform.Find("Button").GetComponent<Button>();
+        craftAxeBTN.onClick.AddListener(delegate { CraftAnyItem(); });
+    }
+    void OpenToolsCategory()
+    {
+        craftingScreenUI.SetActive(false);
+        toolsScreenUI.SetActive(true);
+    }
+    void CraftAnyItem()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U) && !isOpen)
+        {
+
+            Debug.Log("i is pressed");
+            craftingScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isOpen = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.U) && isOpen)
+        {
+            craftingScreenUI.SetActive(false);
+            toolsScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            isOpen = false;
+        }
+    }
+}
---
+++ Assets/Script/InteractableObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public bool playerInRange;
+    public string ItemName;
+
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && playerInRange&& SelectionManager.Instance.onTarget&&SelectionManager.Instance.selectedObject==gameObject)
+        {
+            if (!InventorySystem.Instance.isFull)
+            {
+                InventorySystem.Instance.AddToInventory(ItemName);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("²ËÃ¡¾î");
+            }
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+
+    }
+}
